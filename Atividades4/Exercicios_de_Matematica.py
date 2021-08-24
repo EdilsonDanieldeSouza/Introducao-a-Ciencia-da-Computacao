90e5cf9c1d6a4fa1ee3edc9236e6fa2dbcc45999
edilsondesouzahonda@gmail.com <Edilson>
2021-08-24 15:22:09 -0300

    Exercícios

--- Atividades4/Exercicios_de_Matematica.py
+++ Atividades4/Exercicios_de_Matematica.py
@@ -1,42 +1,29 @@
-#  potencia
-# i = 1
-# while i < 50:
-#     print(2 ** i)
-#     i += 1
+ # potencia
+i = 1
+while i < 50:
+    print(2 ** i)
+    i += 1
 
 # soma
-# inicia = 0
-# quantidade = int(input('Quantos números você quer somar? '))
-# soma = 0
-# while inicia < quantidade:
-#     valor = int(input('Digite o valor a ser somado '))
-#     soma += valor
-#     inicia += 1
-#
-# print('A soma dos valores digitados é:', soma)
+inicia = 0
+quantidade = int(input('Quantos números você quer somar? '))
+soma = 0
+while inicia < quantidade:
+    valor = int(input('Digite o valor a ser somado '))
+    soma += valor
+    inicia += 1
+
+print('A soma dos valores digitados é:', soma)
 
 # produto
-# inicia = 0
-# quantidade = int(input('Quantos números você quer multiplicar? '))
-# produto = 1
-# while inicia < quantidade:
-#     valor = int(input('Digite o valor a ser multiplicado '))
-#     produto *= valor
-#     inicia += 1
-#
-# print('O produto dos valores digitados é:', produto)
+inicia = 0
+quantidade = int(input('Quantos números você quer multiplicar? '))
+produto = 1
+while inicia < quantidade:
+    valor = int(input('Digite o valor a ser multiplicado '))
+    produto *= valor
+    inicia += 1
+
+print('O produto dos valores digitados é:', produto)
 
 # soma do numeros de um valor com varias casas decimais
-
-# numero = input("Dígite um número com vária casas decimais para somar cada número ")
-# i_numero = int(numero)
-# soma = 0
-# i = 0
-# while i < len(numero):
-#     resDiv = i_numero % 10
-#     soma += resDiv
-#     divInt = i_numero // 10
-#     i_numero = divInt
-#     i += 1
-# print(soma)
-
